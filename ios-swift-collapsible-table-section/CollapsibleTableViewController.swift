04e1601558f402616de1e4a9a891517da110edf8
jeantimex <jean.timex@gmail.com>
2016-05-30 10:52:32 -0700

    Added simple example of multi-sections table.

---
+++ ios-swift-collapsible-table-section/CollapsibleTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  CollapsibleTableViewController.swift
+//  ios-swift-collapsible-table-section
+//
+//  Created by Yong Su on 5/30/16.
+//  Copyright © 2016 Yong Su. All rights reserved.
+//
+
+import UIKit
+
+class CollapsibleTableViewController: UITableViewController {
+
+    struct Section {
+        var name: String!
+        var items: [String]!
+    }
+    
+    var sections = [Section]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Initialize the sections array
+        // Here we have three sections: Mac, iPad, iPhone
+        sections = [
+            Section(name: "Mac", items: ["MacBook", "MacBook Air", "MacBook Pro", "iMac", "Mac Pro", "Mac mini", "Accessories", "OS X El Capitan"]),
+            Section(name: "iPad", items: ["iPad Pro", "iPad Air 2", "iPad mini 4", "Accessories"]),
+            Section(name: "iPhone", items: ["iPhone 6s", "iPhone 6", "iPhone SE", "Accessories"])
+        ]
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].name
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].items.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell!
+        cell.textLabel?.text = sections[indexPath.section].items[indexPath.row]
+        return cell
+    }
+    
+}
