68f9269f2c4b1a40dc378c092d03fa32f3753dbf
jeantimex <jean.timex@gmail.com>
2016-05-30 21:00:17 -0700

    Implemented collapsible table section.

--- ios-swift-collapsible-table-section/CollapsibleTableViewController.swift
+++ ios-swift-collapsible-table-section/CollapsibleTableViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
+extension UIView {
+    func rotate(toValue: CGFloat, duration: CFTimeInterval = 0.2, completionDelegate: AnyObject? = nil) {
+        let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        rotateAnimation.toValue = toValue
+        rotateAnimation.duration = duration
+        rotateAnimation.removedOnCompletion = false
+        rotateAnimation.fillMode = kCAFillModeForwards
+        
+        if let delegate: AnyObject = completionDelegate {
+            rotateAnimation.delegate = delegate
+        }
+        self.layer.addAnimation(rotateAnimation, forKey: nil)
+    }
+}
+
 class CollapsibleTableViewController: UITableViewController {
 
+    //
+    // MARK: - Data
+    //
     struct Section {
         var name: String!
         var items: [String]!
+        var collapsed: Bool!
+        
+        init(name: String, items: [String]) {
+            self.name = name
+            self.items = items
+            self.collapsed = false
+        }
     }
     
     var sections = [Section]()
@@ -29,22 +54,49 @@ class CollapsibleTableViewController: UITableViewController {
         ]
     }
     
+    //
+    // MARK: - UITableViewDelegate
+    //
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return sections.count
     }
     
-    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections[section].name
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (sections[section].collapsed!) ? 0 : sections[section].items.count
     }
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sections[section].items.count
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableCellWithIdentifier("header") as! CollapsibleTableViewHeader
+        
+        header.titleLabel.text = sections[section].name
+        header.toggleButton.tag = section
+        header.toggleButton.addTarget(self, action: #selector(CollapsibleTableViewController.toggleCollapse), forControlEvents: .TouchUpInside)
+        
+        header.toggleButton.rotate(sections[section].collapsed! ? 0.0 : CGFloat(M_PI_2))
+        
+        return header.contentView
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell!
+        
         cell.textLabel?.text = sections[indexPath.section].items[indexPath.row]
+        
         return cell
+    }
+    
+    //
+    // MARK: - Event Handlers
+    //
+    func toggleCollapse(sender: UIButton) {
+        let section = sender.tag
+        let collapsed = sections[section].collapsed
+        
+        // Toggle collapse
+        sections[section].collapsed = !collapsed
+        
+        // Reload section
+        tableView.reloadSections(NSIndexSet(index: section), withRowAnimation: .Automatic)
     }
     
 }
---
+++ ios-swift-collapsible-table-section/CollapsibleTableViewHeader.swift
@@ -0,0 +1,16 @@
+//
+//  CollapsibleTableViewHeader.swift
+//  ios-swift-collapsible-table-section
+//
+//  Created by Yong Su on 5/30/16.
+//  Copyright © 2016 Yong Su. All rights reserved.
+//
+
+import UIKit
+
+class CollapsibleTableViewHeader: UITableViewCell {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var toggleButton: UIButton!
+    
+}
